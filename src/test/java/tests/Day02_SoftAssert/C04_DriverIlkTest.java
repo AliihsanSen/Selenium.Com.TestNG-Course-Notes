510f73523e6460ff9e64399e5004033213738f84
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-09-29 12:19:00 +0300

    29.09.2022

--- src/test/java/tests/Day02_SoftAssert/C04_DriverIlkTest.java
+++ src/test/java/tests/Day02_SoftAssert/C04_DriverIlkTest.java
@@ -1,4 +1,22 @@
 package tests.Day02_SoftAssert;
 
+import Utilities.Driver;
+import org.testng.annotations.Test;
+
 public class C04_DriverIlkTest {
+
+    @Test
+    public void test01() {
+
+    /*
+    TestBase class'ına extend ederek kullandığımız driver yerine artık Driver
+    class'ından kullanacağımız getDriver() static method'unu kullanırız
+     */
+        Driver.getDriver().get("https://amazon.com");
+        Driver.getDriver().get("https://bestbuy.com");
+        Driver.closeDriver();
+
+        Driver.getDriver().get("https://hepsiburada.com");
+        Driver.quitDriver();
+    }
 }
