db8b8a0a434ec511b11c550ee8be630ee81aceae
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-01 17:12:53 +0300

    01.10.2022

--- src/test/java/Tests/Day05_SmokeTest/C02_SmokeNegatifTest.java
+++ src/test/java/Tests/Day05_SmokeTest/C02_SmokeNegatifTest.java
@@ -32,10 +32,44 @@ public class C02_SmokeNegatifTest {
 
     @Test
     public void yanlisSifreTest() {
+
+        // https://www.hotelmycamp.com/ adresine git
+        String url = ConfigReader.getProperties("hotelUrl");
+        Driver.getDriver().get(url);
+        HmcPage hotelPages = new HmcPage();
+
+        // login butonuna bas
+        hotelPages.login.click();
+
+        // test data username: manager ,  test data password : manager1
+        hotelPages.userName.sendKeys("manager");
+        hotelPages.password.sendKeys("manager1");
+        hotelPages.loginButton.click();
+
+        // Degerleri girildiginde sayfaya girilemedigini test et
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(hotelPages.girisYapilamadi.isDisplayed());
     }
 
     @Test
     public void yanlisKullaniciveSifreTest() {
+
+        // https://www.hotelmycamp.com/ adresine git
+        String url = ConfigReader.getProperties("hotelUrl");
+        Driver.getDriver().get(url);
+        HmcPage hotelPages = new HmcPage();
+
+        // login butonuna bas
+        hotelPages.login.click();
+
+        // test data username: manager1 ,  test data password : manager1
+        hotelPages.userName.sendKeys("manager1");
+        hotelPages.password.sendKeys("manager1");
+        hotelPages.loginButton.click();
+
+        // Degerleri girildiginde sayfaya girilemedigini test et
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(hotelPages.girisYapilamadi.isDisplayed());
 
     }
 }
