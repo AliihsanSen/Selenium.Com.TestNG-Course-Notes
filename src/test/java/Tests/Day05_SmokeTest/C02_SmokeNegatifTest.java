d0426c355a97744b6d8bb77b7b027210b7f9f4bc
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-01 17:10:39 +0300

    01.109.2022

---
+++ src/test/java/Tests/Day05_SmokeTest/C02_SmokeNegatifTest.java
@@ -0,0 +1,41 @@
+package Tests.Day05_SmokeTest;
+
+import Pages.HmcPage;
+import Utilities.ConfigReader;
+import Utilities.Driver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class C02_SmokeNegatifTest {
+
+    @Test
+    public void yanlisKullaniciTest() {
+
+        // https://www.hotelmycamp.com/ adresine git
+        String url = ConfigReader.getProperties("hotelUrl");
+        Driver.getDriver().get(url);
+        HmcPage hotelPages = new HmcPage();
+
+        // login butonuna bas
+        hotelPages.login.click();
+
+        // test data username: manager1 ,  test data password : manager1!
+        hotelPages.userName.sendKeys("manager1");
+        hotelPages.password.sendKeys("Manager1!");
+        hotelPages.loginButton.click();
+
+        // Degerleri girildiginde sayfaya girilemedigini test et
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(hotelPages.girisYapilamadi.isDisplayed());
+
+    }
+
+    @Test
+    public void yanlisSifreTest() {
+    }
+
+    @Test
+    public void yanlisKullaniciveSifreTest() {
+
+    }
+}
