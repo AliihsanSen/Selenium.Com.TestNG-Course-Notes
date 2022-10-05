2f9014ffb1ab158fb4b55454bc32124afbb1bfef
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-05 23:02:27 +0300

    05.10.2022

--- src/test/java/Tests/Day06_ReusableMethods/C02_WindowHandlesReusableMethod.java
+++ src/test/java/Tests/Day06_ReusableMethods/C02_WindowHandlesReusableMethod.java
@@ -1,6 +1,7 @@
 package Tests.Day06_ReusableMethods;
 
 import Utilities.Driver;
+import Utilities.ReusableMethods;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -27,5 +28,21 @@ public class C02_WindowHandlesReusableMethod {
         Driver.closeDriver();
     }
 
+    @Test
+    public void test02() {
 
+        // ● https://the-internet.herokuapp.com/windows adresine gidin.
+        Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
+
+        // ● Click Here butonuna basın.
+        ReusableMethods.waitFor(3);
+        Driver.getDriver().findElement(By.xpath("//*[text()='Click Here']")).click();
+
+        // acilan yeni tab'in title'inin "New Window" oldugunu test edin
+        ReusableMethods.switchToWindow("New Window");
+        String expectedTitle = "New Window";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+        Driver.quitDriver();
+    }
 }
