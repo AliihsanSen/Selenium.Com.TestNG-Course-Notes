7bee958476c6ddfaca173058c3353f0b9a870c79
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-10 18:17:09 +0300

    10.10.2022

--- src/test/java/Tests/Practice03/Q01_.java
+++ src/test/java/Tests/Practice03/Q01_.java
@@ -22,13 +22,15 @@ public class Q01_ {
     @Test
     public void testName() throws InterruptedException {
 
-        //Go to https://testpages.herokuapp.com/styled/events/javascript-events.html
+        // Go to https://testpages.herokuapp.com/styled/events/javascript-events.html
         Driver.getDriver().get("https://testpages.herokuapp.com/styled/events/javascript-events.html");
-        //Click all the buttons and verify they are all clicked
+
+        // Click all the buttons and verify they are all clicked
         herokuTestPage = new HerokuTestPage();
-        //kodlarımız yazarken clean code kapsamında daha sade kod yazabilmek amacı ile her test methodu içerisinde
-        //ayrı bir object create etmektense bu objecti class seviyesinde instance olarak create edip
-        //test methodlarında buna değer atamak ve kullanmak daha uygun bir yöntemdir
+
+        // kodlarımız yazarken clean code kapsamında daha sade kod yazabilmek amacı ile her test methodu içerisinde
+        // ayrı bir object create etmektense bu objecti class seviyesinde instance olarak create edip
+        // test methodlarında buna değer atamak ve kullanmak daha uygun bir yöntemdir
         herokuTestPage.onblur.click();
         herokuTestPage.onclick.click();
         herokuTestPage.onclick.click();
@@ -47,6 +49,7 @@ public class Q01_ {
                 click(herokuTestPage.mouseDown).perform();
         Thread.sleep(2000);
     }
+
     @Test (dependsOnMethods = "test01")
     public void test02() {
         List<WebElement> clicked = herokuTestPage.eventTriggered;
