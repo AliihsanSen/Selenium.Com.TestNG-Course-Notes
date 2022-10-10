d3f3b727343e6b9a899df586be9a213c924ded3d
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-10 18:14:17 +0300

    10.10.2022

--- src/test/java/Pages/HerokuTestPage.java
+++ src/test/java/Pages/HerokuTestPage.java
@@ -1,32 +1,47 @@
 package Pages;
 
+import Utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class HerokuTestPage {
 
     public HerokuTestPage (){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
     @FindBy (xpath = "//button[@id='onblur']")
     public WebElement onblur;
+
     @FindBy (xpath = "//button[@id='onclick']")
     public WebElement onclick;
+
     @FindBy (xpath = ("//button[@id='oncontextmenu']"))
     public WebElement contextmenu;
+
     @FindBy (xpath = ("//button[@id='ondoubleclick']"))
     public WebElement doubleClick;
+
     @FindBy (xpath = ("//button[@id='onfocus']"))
     public WebElement onfocus;
+
     @FindBy(xpath = ("//button[@id='onkeydown']"))
     public WebElement keydown;
+
     @FindBy (xpath = ("//button[@id='onkeyup']"))
     public WebElement keyup;
+
     @FindBy (xpath = ("//button[@id='onkeypress']"))
     public WebElement keypress;
+
     @FindBy (xpath = ("//button[@id='onmouseover']"))
     public WebElement mouseOver;
+
     @FindBy (xpath = ("//button[@id='onmouseleave']"))
     public WebElement mouseLeave;
+
     @FindBy (xpath = ("//button[@id='onmousedown']"))
     public WebElement mouseDown;
+
     @FindBy (xpath = ("//*[.='Event Triggered']"))
     public List<WebElement> eventTriggered;
 
