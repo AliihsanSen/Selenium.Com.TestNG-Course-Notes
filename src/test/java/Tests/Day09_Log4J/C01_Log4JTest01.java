d07b88e00d9b32edaf7e8fbb56c55e0d51e76d66
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-10 12:13:37 +0300

    10.10.2022

--- src/test/java/Tests/Day09_Log4J/C01_Log4JTest01.java
+++ src/test/java/Tests/Day09_Log4J/C01_Log4JTest01.java
@@ -2,8 +2,17 @@ package Tests.Day09_Log4J;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
 
 public class C01_Log4JTest01 {
 
     private static Logger logger = LogManager.getLogger(C01_Log4JTest01.class.getName());
+
+    @Test
+    public void log4jTest1() {
+        logger.info("LOG INFO");
+        logger.debug("LOG DEBUG");
+        logger.error("LOG ERROR");
+        logger.fatal("LOG FATAL");
+    }
 }
