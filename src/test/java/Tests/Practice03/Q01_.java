2f515d435ba489f3ca6206914fe48cc2bee17329
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-10 18:15:38 +0300

    10.10.2022

--- src/test/java/Tests/Practice03/Q01_.java
+++ src/test/java/Tests/Practice03/Q01_.java
@@ -1,4 +1,20 @@
 package Tests.Practice03;
 
+import Pages.HerokuTestPage;
+import Utilities.Driver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
 public class Q01_ {
+
+    // Go to https://testpages.herokuapp.com/styled/events/javascript-events.html
+    // Click all the buttons and verify they are all clicked
+    
+    HerokuTestPage herokuTestPage;
+    
+    Actions actions = new Actions(Driver.getDriver());
+
+    @Test
+    public void testName() {
+    }
 }
