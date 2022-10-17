34fd76e8009b1427df93636009733cba800596d8
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-17 14:58:45 +0300

    17.10.2022

--- src/test/java/Tests/Day09_Log4J/C02_Log4JTest02.java
+++ src/test/java/Tests/Day09_Log4J/C02_Log4JTest02.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class C01_Log4JTest02 {
+public class C02_Log4JTest02 {
 
     private static Logger logger = LogManager.getLogger(C01_Log4JTest01.class.getName());
 
