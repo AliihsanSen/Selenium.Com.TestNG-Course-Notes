1b3c73d5d0cce83e814025816d8c6eda1f4b3762
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-17 15:07:34 +0300

    17.10.2022

--- src/test/java/Tests/Day06_ReusableMethods/C01_ScreenShotReusableMethod.java
+++ src/test/java/Tests/Day06_ReusableMethods/C01_ScreenShotReusableMethod.java
@@ -1,18 +1,27 @@
 package Tests.Day06_ReusableMethods;
 
 import Pages.AmazonPage;
+import Tests.Day09_Log4J.C01_Log4JTest01;
 import Utilities.ConfigReader;
 import Utilities.Driver;
 import Utilities.ReusableMethods;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class C01_ScreenShotReusableMethod {
+
+    private static Logger logger = LogManager.getLogger(C01_ScreenShotReusableMethod.class.getName());
 
     @Test
     public void testName() throws IOException {
+
 
         // Hepsiburada sayfasına gidiniz
         Driver.getDriver().get("https://hepsiburada.com");
@@ -35,7 +44,7 @@ public class C01_ScreenShotReusableMethod {
         amznPage.aramaKutusu.sendKeys("Nutella", Keys.ENTER);
 
         // Arama sonuc yazısı Webelementinin resmini alınız
-        ReusableMethods.getScreenshotWebElement("AramaSonucWE",amznPage.aramaSonucWE);
+        ReusableMethods.getScreenshotWebElement("AramaSonucWE", amznPage.aramaSonucWE);
 
     }
 }
