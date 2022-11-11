01535944a30301993c6d8837a46014a2eff1e9a8
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-11-12 00:07:17 +0300

    12.11.2022

--- src/test/java/Tests/Day05_SmokeTest/SeleniumGrid2.java
+++ src/test/java/Tests/Day05_SmokeTest/SeleniumGrid2.java
@@ -18,7 +18,7 @@ public class SeleniumGrid2 {
     @Test
     public void testName() throws InterruptedException, MalformedURLException {
 
-        driver = new RemoteWebDriver(new URL("http://192.168.1.4:4444"),new FirefoxOptions());
+        driver = new RemoteWebDriver(new URL("http://192.168.1.4:4444"),new EdgeOptions());
 
         driver.get("https://www.bluerentalcars.com/");
         Thread.sleep(3000);
